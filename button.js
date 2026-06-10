10cd69417fa84573a9858b01df2eb849ff2b166e
gegxkss <ksyusha.lge@gmail.com>
2026-06-10 15:58:23 +0300

    fixed after first code review

--- button.js
+++ button.js
@@ -6,15 +6,16 @@ const orderForm = document.getElementById('coffeeOrderForm');
 if (openPopupBtn && orderPopup) {
     openPopupBtn.addEventListener('click', () => {
         console.log('попап открыт');
-        orderPopup.style.display = 'flex';
+        orderPopup.classList.toggle('active');
+        openPopupBtn.classList.toggle('active');
     });
 }
 
 if (orderForm && orderPopup) {
     orderForm.addEventListener('submit', () => {
         const coffee = document.getElementById('coffeeType').value;
-        const name = document.getElementById('userName').value;
-        const phone = document.getElementById('userPhone').value;
+        const name = document.getElementById('nameInputValue').value;
+        const phone = document.getElementById('phoneInputValue').value;
         console.log(`Заказ ${coffee}, Имя ${name}, Телефон ${phone}`);
         console.log(`Ваш заказ принят`);
 
